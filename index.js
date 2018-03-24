a149172f1462f0317d07e1b51f4a4adc208272df
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-24 17:55:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function shout(string) {
   return string.toUpperCase()
 }
-shout(hello!)
+shout('hellooo dolly!')
 
 function whisper(string) {
   return string.toLowerCase()
 }
-whisper(hello!)
+whisper('hi there!')
 
 function logShout(string) {
   console.log(string.toUpperCase)
 }
+logshout('Bonjourno principesa!')
