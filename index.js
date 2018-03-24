93dbc1d2d79bc8d79cb204a8b8572526bf81461e
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-24 18:13:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 function shout(string) {
   return string.toUpperCase()
 }
-shout('hellooo dolly!')
+shout('hellooo dolly!');
 
 function whisper(string) {
   return string.toLowerCase()
 }
-whisper('hi there!')
+whisper('hi there!');
 
 function logShout(string) {
   console.log(string.toUpperCase)
 }
-logShout('Bonjourno principesa!')
+logShout('Bonjourno principesa!');
 
 function logWhisper(string) {
   console.log(string.toLowerCase)
 }
-logWhisper('you have toilet paper stuck to your shoe!')
+logWhisper('you have toilet paper stuck to your shoe!');
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
+  if (string === string.toLowerCase())
     return "I can't hear you"!";
-  } else if (string === string.toUpperCase())
-}
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if ('I love you, Grandma.') {
+    return 'I love you, too.'
+  }
